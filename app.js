110a90abaeb7a1cf4e9cd24e59e5a971942567ac
Lucas <32420616+QuixoteCode@users.noreply.github.com>
2024-07-08 19:32:48 +0200

    4:28:30

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const http = require('http')
 
 const server = http.createServer((req, res)=>{
-    console.log('user hit the server');
-    res.end('home page')
+    //console.log(req.method)
+    console.log(req.url)
+    res.writeHead(200,{'content-type':'text/html'})
+    res.write('<h1>home page</h1>')
+    res.end()
 })
 
 server.listen(5000)
