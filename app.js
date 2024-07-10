14bb3362aab9ae03c3fafc38ae9b9799651a6240
Lucas <32420616+QuixoteCode@users.noreply.github.com>
2024-07-10 17:00:59 +0200

    6:12:00

--- app.js
+++ app.js
@@ -24,8 +24,33 @@ app.get('/api/products/:productID', (req,res)=>{
     if(!singleProduct){
         return res.status(404).send('Product Does Not Exist')
     }
-    console.log(singleProduct)
-    res.json(singleProduct)
+    return res.json(singleProduct)
+})
+
+app.get('/api/products/:productID/reviews/:reviewID',(req,res)=>{
+    console.log(req.params)
+    res.send('hello world')
+})
+
+app.get('/api/v1/query', (req,res)=>{
+    //console.log(req.query)
+    const {search,limit}=req.query
+    let sortedProducts = [...products];
+    
+    if(search){
+        sortedProducts = sortedProducts.filter((product)=>{
+            return product.name.startsWith(search)
+        })
+    }
+    if(limit){
+        sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+    if(sortedProducts.length < 1){
+        //res.status(200).send('no products matched your sarch')
+        return res.status(200).json({success:true, data: []})
+    }
+    res.status(200).json(sortedProducts)
+
 })
 
 app.listen(5000, ()=>{
