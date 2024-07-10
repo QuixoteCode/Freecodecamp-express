1c2cd10e51474b5174f4b1474f7e9c7437562d83
Lucas <32420616+QuixoteCode@users.noreply.github.com>
2024-07-10 15:00:50 +0200

    5:48:15

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
+const {products} = require('./data.js')
 
-app.use(express.static('./public'))
-
-app.get('/', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+app.get('/',(req,res)=>{
+    res.send('<h1>Home page</h1><a href="/api/products">products</a>')
+})
+app.get('/api/products',(req,res)=>{
+    const newProducts = products.map((product)=>{
+        const {id,name,image} = product;
+        return {id,name,image}
+    })
+    res.json(newProducts)
 })
 
-app.all('*', (req,res)=>{
-    res.status(404).send('resource not found')
+app.get('/api/products/:productID', (req,res)=>{
+    //console.log(req)
+    //console.log(req.params)
+    const {productID} = req.params
+
+    const singleProduct = products.find(
+        (product)=> product.id === Number(productID)
+    )
+    if(!singleProduct){
+        return res.status(404).send('Product Does Not Exist')
+    }
+    console.log(singleProduct)
+    res.json(singleProduct)
 })
 
 app.listen(5000, ()=>{
-    console.log('server is listening on port 5000....')
+    console.log('Server is listening on port 5000...')
 })
