ef0b31cdfcaf61693c4f377cac41f2b6581ba549
Lucas <32420616+QuixoteCode@users.noreply.github.com>
2024-07-12 19:48:02 +0200

    7:01:30

--- app.js
+++ app.js
@@ -1,28 +1,19 @@
 const express = require('express')
 const app = express()
-const morgan = require('morgan')
-const logger = require('./logger')
-const authorize = require('./authorize')
-// req => middleware => res
+let {people} = require('./data')
 
-//1. use vs route
-//2. options - our own / express / third party
+// static assets
+app.use(express.static('./methods-public'))
+// parse form data
+// diferente a lo proporcionado en el tutorial: https://stackoverflow.com/questions/67764774/typeerror-bodyparser-urlencoded-is-not-a-function
+app.use(express.urlencoded())
+app.get('/api/people',(req,res)=>{
+    res.status(200).json({success:true,data:people})
+})
 
-//app.use([logger,authorize])
-//app.use(express.static('./public'))
-app.use(morgan('tiny'))
-app.get('/', (req, res)=>{
-    res.send('Home')
-})
-app.get('/about', (req, res) =>{
-    res.send('About page')
-})
-app.get('/api/products', (req, res) =>{
-    res.send('Products')
-})
-app.get('/api/items', [logger, authorize], (req, res) =>{
-    console.log(req.user);
-    res.send('Items')
+app.post('/login',(req, res) => {
+    console.log(req.body)
+    res.send('POST ')
 })
 
 app.listen(5000, () => {
