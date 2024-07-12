f74ceb99cdeed6cf293f72914747ccbe28f0753b
Lucas <32420616+QuixoteCode@users.noreply.github.com>
2024-07-12 19:19:50 +0200

    6:43:30

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 const logger = require('./logger')
 const authorize = require('./authorize')
 // req => middleware => res
-app.use([logger,authorize])
 
+//1. use vs route
+//2. options - our own / express / third party
+
+//app.use([logger,authorize])
+//app.use(express.static('./public'))
+app.use(morgan('tiny'))
 app.get('/', (req, res)=>{
     res.send('Home')
 })
@@ -14,7 +20,7 @@ app.get('/about', (req, res) =>{
 app.get('/api/products', (req, res) =>{
     res.send('Products')
 })
-app.get('/api/items', (req, res) =>{
+app.get('/api/items', [logger, authorize], (req, res) =>{
     console.log(req.user);
     res.send('Items')
 })
